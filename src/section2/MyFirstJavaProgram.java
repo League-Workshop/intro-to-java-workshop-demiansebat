c86080e60aa369c5945eb77314c023560067e5cc
demiansebat <league@iMac-2.attlocal.net>
2018-07-10 12:01:08 -0700

    day 2 projects

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,12 +4,18 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot java = new Robot();
+		java.setSpeed(200);
+		java.penDown();
+		java.setPenColor(Color.CYAN);
+		for (int i = 0; i < 4; i++) {
+			java.move(200);
+			java.turn(140);
+		}
+
 	}
 }
--- src/section2/PentagonCrazy.java
+++ src/section2/PentagonCrazy.java
@@ -10,32 +10,37 @@ public class PentagonCrazy {
 
 	private void makePrettyThings() {
 		// 1. Create a new Robot
-
+		Robot meh = new Robot();
 		// 3. Put the robot's pen down
-
+		meh.penDown();
 		// 8. Make the robot go at maximum speed (100)
-
+		meh.setSpeed(100);
 		// 9. Set the pen to a color that you like for the shape
-
+		meh.setPenColor(Color.GREEN);
 		// 4. Make a variable for the number of sides you want (can’t test this one)
-
-		// 5. Make a second variable for the angle you want the robot to turn. Hint: you can divide in Java using "/". Can’t test until step 6
-
-		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you should see a pentagon
-
-			// 2. Move the robot 200 pixels
-	
-			// 10. Change the previous line of code to make the robot move "i" pixels instead of 200 
-	
-			// 6. Turn the robot the amount in your angle variable
-	
-			// 11. Turn the robot one more degree
-
+		int sides = 5;
+		// 5. Make a second variable for the angle you want the robot to turn. Hint: you
+		// can divide in Java using "/". Can’t test until step 6
+		int angles = 5;
+		// 7. Use a for loop to repeat steps #2 to #11, 200 times. When this is done you
+		// should see a pentagon
+		for (int i = 0; i < 200; i++) {
+		// 2. Move the robot 200 pixels
+		// meh.move(200);
+		// 10. Change the previous line of code to make the robot move "i" pixels
+		// instead of 200
+		meh.move(i);
+		// 6. Turn the robot the amount in your angle variable
+		meh.turn(5);
+		// 11. Turn the robot one more degree
+meh.turn(1);
 	}
+	}
+		
 
 	// Variations:
 	// *12. make the pattern really huge
-	// *13. randomize the color of the pattern 
+	// *13. randomize the color of the pattern
 	// *14. experiment with different shapes
 
 	public static void main(String[] args) {
