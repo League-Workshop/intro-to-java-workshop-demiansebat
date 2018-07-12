1931275793df564cd5a6b1b366ddde18af3f9a20
demiansebat <league@iMac-2.attlocal.net>
2018-07-12 12:01:06 -0700

    day 4

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,22 +1,30 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
-	
+
 	public static void main(String[] args) {
-		
-		// 1.  Create a variable to hold the user's score 
-		
-		// 2.  Ask the user a question 
-		
-		// 3.  Use an if statement to check if their answer is correct
-		
-		// 4.  if the user's answer is correct
-		
-		// -- add one to their score 
-		
-		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
-		// 6.  After all the questions have been asked, print the user's score 
-		
+
+		// 1. Create a variable to hold the user's score
+		int score = 0;
+		// 2. Ask the user a question
+		String answer = JOptionPane
+				.showInputDialog("how many years would it take to travel all the way around the universe ");
+		// 3. Use an if statement to check if their answer is correct
+		if (answer.equals("93 billion")) {
+			score++;
+
+		}
+		String answer2 = JOptionPane.showInputDialog("how many feet long is a regular persons small intestine");
+		// 4. if the user's answer is correct
+		if (answer2.equals("22")) {
+			// -- add one to their score
+			score++;
+		}
+		// 5. Create more questions by repeating steps 1, 2, and 3 below.
+
+		// 6. After all the questions have been asked, print the user's score
+
 	}
 }
